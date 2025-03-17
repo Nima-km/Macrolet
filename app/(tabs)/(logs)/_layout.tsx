f9a4668aaf5520d90f8316743481f809fe05a280
Nima <n.karimi206@gmail.com>
2025-03-17 12:12:42 -0400

    initiates logFood implementation

--- app/(tabs)/(logs)/_layout.tsx
+++ app/(tabs)/(logs)/_layout.tsx
@@ -4,6 +4,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="logs" options={{ headerShown: false }} />
+      <Stack.Screen name="addFood" options={{ headerShown: false }} />
       <Stack.Screen name="logFood" options={{ headerShown: false }} />
     </Stack>
   );
